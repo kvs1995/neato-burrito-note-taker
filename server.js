5c3ec1bba6f6d55b5d8a8bfac8146bacc6a8b224
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-13 22:00:05 -0500

    added code in for server, the middleware and the get request for the homepage

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 // import libraries, express
-//require fs and util and uuid
+const express = require('express');
 //need to import path so the new notes can be written to the json file
-
+const path = require('path');
 //import the index.js from routes
-
+const api = require('./routes/index.js')
 //import helper methods
+const uuid = require('./helpers/uuid.js');
 
 //set PORT 
+const PORT = process.env.PORT || 3001;
 
+const app = express();
 //middleware for parsingJSOn and urlendcoded for data
 //use express.json()
-
+app.use(express.json());
 //use express.urlencoded({ extended: true })
-
+app.use(express.urlencoded({ extended: true }));
 //set initial url to '/api'
-
+app.use('/api', api)
 //middleware to make public folder static
 //express.static('public')
+app.use(express.static('public'));
 
 //GET route for homepage 
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 //app to listen to the port
-
+app.listen(PORT, () => 
+  console.log(`App listening at http://localhost:${PORT}`)
+);
 
