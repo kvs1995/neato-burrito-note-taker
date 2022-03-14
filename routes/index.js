a07d9195d9dbaada8b1bdcdbd2156c045cf76532
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-13 22:04:27 -0500

    added in require statmements and middleware to use the notes path

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 //import libraries: express. 
-
-//set app variable to express ()
+const express = require('express');
 
 //require the notes route file
+const notesRouter = require('./notes');
+
+//set app variable to express ()
+const app = express();
 
 //middleware to use '/notes' path as the url address set to the notes variables
+app.use('/notes', notesRouter);
 
 //export the module app
-
+module.exports = app;
