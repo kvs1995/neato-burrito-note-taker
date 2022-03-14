98e9776685f4cb5a3346a33720bd660472e041ab
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-13 22:46:35 -0500

    added in detailed pseudocode for the notes.js route file for each of the routes to get a specific id, a delete route, and a post route

--- routes/notes.js
+++ routes/notes.js
@@ -14,12 +14,45 @@ const notesData = require('../db/db.json')
 notes.get('/', (req, res) => {
   fs.readFile(notesData).then((data) => res.json(JSON.parse(data)));
 });
+
 //GET route for a specific note
+//path set to '/:id'
+  //set notesID = what is passed in to the request id parameters
+  //fs.readFile(notesData) and parse
+  //then take the json parsed data
+    //set the result equal to the json data filtered to the id 
+    //return if the result.length is greater than 0, the res result, else return a statment saying there are no notes yet. 
 
 //DELETE route for a specific note
+//path set to '/:id
+  //set noteID = the target parameters id
+  //fs.readFile(notesData)
+    //then take data and parse
+    //THEN with the parsed json data
+      //make a new array of all notes except the one with ID provided in the URL
 
+      //save that array to the filesystem (fs.writeToFile(notesData), the new array)
+
+      //add in a response to let the user know that the data attached to the id has been deleted
 
 //POST  route for a new note
+//path set to '/'
+  //set the {title, text} equal to the req.body
+  //if the req body is true (has data in it) 
+    //set newNote = title, text and id: uuid()
+
+    //read and append the newNote to the notesData
+    //readFile(notesData)
+      //then parse Data
+      //then take the json data 
+        //set the set result equal to an array (json.map(note => note)
+
+        //push the new Note to the result
+
+        //write the new array with the new note to the json data file. 
+
+        //add in a response to let the user know that the note has been appended
+    //else return error message that an issue occrred with adding the note.
 
 //export the module notes
 
