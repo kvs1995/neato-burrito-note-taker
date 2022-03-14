4217969cf7a4d6e47f2dfc97c4b0e78bc76f303b
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-13 21:12:44 -0500

    pseudo code for the server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+// import libraries, express
+//need to import path so the new notes can be written to the json file
+
+//import the index.js from routes
+
+//import helper methods
+
+//set PORT 
+
+//middleware for parsingJSOn and urlendcoded for data
+
+//middleware to make public folder static
+
+//GET route for homepage 
+
+//app to listen to the port
