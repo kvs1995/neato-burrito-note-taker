76c570507ca3240f746f47d456e8aed21ba0536d
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-13 21:22:10 -0500

    psuedocode for routes/notes.js

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+//set notes variable to require express as Router
+
+
+//require the helper utilities to write to the json file
+
+//GET route for retreiving all the notes
+
+//GET route for a specific note
+
+//DELETE route for a specific note
+
+
+//POST  route for a new note
+
+//export the module notes
